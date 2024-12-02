534d1a309b0df8c2f62997d9bdf7c7db3c266178
HM2811 <mekkaoui151@gmail.com>
2024-12-02 13:33:38 +0100

    Complete TP: Added SSL Client-Server setup

---
+++ SSLClient.java
@@ -0,0 +1,22 @@
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+public class SSLClient {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", "clientcert.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "clientpass");
+        System.setProperty("javax.net.ssl.trustStore", "clientTruststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "truststorepass");
+
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket("localhost", 8443);
+
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        out.println("Hello, server!");
+        String response = in.readLine();
+        System.out.println("Received: " + response);
+    }
+}
